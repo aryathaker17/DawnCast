fbe400300465130105cc70d9e959e19a2c14c4a6
Arya Thaker <aryathaker7@gmail.com>
2026-03-25 00:55:15 -0400

    Initial Commit

---
+++ DawnCast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DawnCast
+//
+//  Created by Arya Thaker on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DawnCast/DawnCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  DawnCastApp.swift
+//  DawnCast
+//
+//  Created by Arya Thaker on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct DawnCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
